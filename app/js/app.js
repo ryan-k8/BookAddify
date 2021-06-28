655871d189902e64c72ea0d2799c42ca0ac7f90a
ryan-school2020 <ryan.khursheedps@gmail.com>
2021-06-28 15:40:56 +0530

    added static ui of project

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAJH95UxbtGoMW9Ucj0ztsskXZzcX8MzPk",
+  authDomain: "bookaddify.firebaseapp.com",
+  projectId: "bookaddify",
+  storageBucket: "bookaddify.appspot.com",
+  messagingSenderId: "1031147019594",
+  appId: "1:1031147019594:web:11cb1dfc09e71734aff5b3",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
