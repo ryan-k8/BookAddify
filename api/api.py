e671b93ac153ca803c697c4ee3d49abdb063cf86
ryan-school2020 <ryan.khursheedps@gmail.com>
2021-07-04 15:38:47 +0530

    some cleaning of data sent by flask api

--- api/api.py
+++ api/api.py
@@ -9,31 +9,48 @@ def book_scrape(book_name):
 
     # the first result's url (for project anyway)
     most_relevant = soup.find('h3', {'itemprop': 'name'}).find('a')['href']
-
     response_ = requests.get(f'{BASE_URL+most_relevant}').text
     soup_ = BeautifulSoup(response_, 'html.parser')
 
-    book_title = soup_.find('h1', {'itemprop': 'name'}).contents[0]
+    book_title = soup_.find('h1', {'itemprop': 'name'}).contents[0].split()
+    book_title_clean = ''
+    for i in book_title:
+        book_title_clean += f'{i} '
+
     book_author = soup_.find('a', {'itemprop': 'author'}).contents[0]
     book_publisher = soup_.find(
         'div', {'class': 'bookProperty property_publisher'}).find('div', {'class': {'property_value'}}).contents[0]
     book_release_yr = soup_.find('div', {'class': 'bookProperty property_year'}).find(
         'div', {'class': 'property_value'}).contents[0]
-    book_summary = soup_.find('div', {'itemprop': 'reviewBody'}).contents
+    book_summary = soup_.find('div', {'itemprop': 'reviewBody'})
+    if (book_summary is None):
+        book_summary = ''
+    else:
+        book_summary = book_summary.contents[1:]
+        book_summary_clean = ''
+        for i in book_summary:
+            book_summary_clean += f'{i} '
     book_img = soup_.find('div', {'class': 'z-book-cover'}).find('img')['src']
-
     # primary dwnld
     book_dwnld_primary = soup_.find('a', {'class': 'addDownloadedBook'})
-    book_dwnld_title = book_dwnld_primary.contents[2]
-    book_dwnld_src = BASE_URL + book_dwnld_primary['href']
-    book_dwnld_lst = [book_dwnld_title, book_dwnld_src]
+    if (book_dwnld_primary is None):
+        book_dwnld_title = 'null'
+        book_dwnld_src = 'null'
+    else:
+        book_dwnld_title = book_dwnld_primary.contents[2].split()
+        book_dwnld_title_clean = ''
+        for i in book_dwnld_title:
+            book_dwnld_title_clean += f'{i} '
+        book_dwnld_src = BASE_URL + book_dwnld_primary['href']
+
+    book_dwnld_lst = [book_dwnld_title_clean, book_dwnld_src]
 
     RESULT = {
-        'book': book_title,
+        'book': book_title_clean,
         'author': book_author,
         'publisher': book_publisher,
         'release': book_release_yr,
-        'summary': str(book_summary),
+        'summary': book_summary_clean,
         'resources': {
             'image': book_img,
             'downloads': book_dwnld_lst
